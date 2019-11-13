5385dc940212ecca4acca5c5ec965412ec9f5645
Martin Nohava <martin.nohava.mn@gmail.com>
2019-11-13 17:35:47 +0100

    Final of ExtEuclAlg

--- functions.py
+++ functions.py
@@ -7,7 +7,7 @@ def isPrime(x):
 def factorization(x):
     factors = []
     for factor in range(3,x,2):
-        print(factor)
+        print("Faktorizace")
         if x%factor == 0:
             factors.append(factor)
             if len(factors) == 2:
@@ -24,7 +24,7 @@ def inversion(public_key, PHI):
             if (public_key * private_key)%PHI == 1:
                 return private_key
             else:
-                print(private_key)
+                print("Inverze")
                 private_key += 1
 
 def eulerInversion(public_key, PHI):
