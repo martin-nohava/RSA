cf565becb64cd50a69c21c92e18780e61d83f54d
Martin Nohava <martin.nohava.mn@gmail.com>
2019-11-22 16:09:21 +0100

    Basic exception catching

--- RSA.py
+++ RSA.py
@@ -2,8 +2,8 @@ import time
 from yaspin import yaspin
 from prettytable import PrettyTable
 from functions import *
-import multiprocessing
 
+# Only main thread can access this code 
 if __name__ == "__main__":
     print("""
       ____  ____    _    
@@ -14,35 +14,68 @@ if __name__ == "__main__":
     --public key cracking--                    
                         """)
 
-    #vkládané parametry
-    print("---enter parametrs---")
-    public_key = int(input("Enter public key: "))
-    modulo = int(input("Enter modulo: "))
-    input("-press enter to begin-")
-    print("\n")
+    # Inserting parameters
+    while True:
+        print("---enter parametrs---")
+        public_key = input("Enter public key: ")
+        modulo = input("Enter modulo: ")
+        input("-press enter to begin-")
+        print("\n")
+
+        # Catching invalid input
+        if numCheck(modulo, public_key) == True:
+            public_key = int(public_key)
+            modulo = int(modulo)
+            break
+        print("\033[;33mError: All input parameters must be numbers!\033[0;0m\n")
     
-    #zjistime r, s
+    # Finding p, q
     with yaspin(text="Finding primes...", color="yellow") as spinner:
         factors, factorization_time = factorization_switch(modulo)
         spinner.ok("✔ \033[0;32m[Done]\033[0;0m")
         print("\033[1;30m  [" + str(factorization_time) + "]  Time for execution\033[0;0m")
 
-    #zjistime PHI
+    # Back check before continuing
+    with yaspin(text="Checking values...", color="yellow") as spinner:
+        start_time = time.time()
+        if isPrime(factors[0]) == False or isPrime(factors[1]) == False:
+            print("\n\nFatal Error: Modulo is not created by two prime factors!\n")
+            os._exit(0)
+        if factors[0] * factors[1] != modulo:
+            print("\n\nFatal Error: Failed to find factors!\n")
+            os._exit(0)
+        end_time = time.time()
+        final_time = end_time - start_time
+        spinner.ok("✔ \033[0;32m[Done]\033[0;0m")
+        print("\033[1;30m  [" + str(final_time) + "]  Time for execution\033[0;0m")
+
+    # Computing PHI
     PHI = (factors[0]-1) * (factors[1]-1)
         
-    #zjistim private_key w/ euclid
-    with yaspin(text="Finding private key...", color="yellow") as spinner:
+    # Generating private key
+    with yaspin(text="Generating private key...", color="yellow") as spinner:
         start_time = time.time()
         not_inversion, private_key = extendedEuclidianAlgirithmInversion(PHI, public_key)
         end_time = time.time()
-        inversion_time = end_time - start_time
+        final_time = end_time - start_time
         spinner.ok("✔ \033[0;32m[Done]\033[0;0m")
-        print("\033[1;30m  [" + str(inversion_time) + "]  Time for execution\033[0;0m")
+        print("\033[1;30m  [" + str(final_time) + "]  Time for execution\033[0;0m")
 
-    #oprava znamenka
-    private_key = signCheck(private_key, public_key, PHI)    
+    # Converting negative private key to positive value
+    private_key = signCheck(private_key, public_key, PHI)  
 
-    #output log
+    # Back check before continuing
+    with yaspin(text="Checking private key...", color="yellow") as spinner:
+        start_time = time.time()
+        if (private_key * public_key) % PHI != 1:
+            print("\n\nFatal Error: Failed to generate valid private key!\n")
+            os._exit(0)
+        end_time = time.time()
+        final_time = end_time - start_time
+        spinner.ok("✔ \033[0;32m[Done]\033[0;0m")
+        print("\033[1;30m  [" + str(final_time) + "]  Time for execution\033[0;0m")
+
+    # Outputing all vulues for user 
     print("\n")
     output = PrettyTable()
     output.field_names = ["OUTPUT TABLE", " "]
@@ -52,4 +85,5 @@ if __name__ == "__main__":
     print (output)
     print("\033[0;0m\n")
 
+    # Exit
     input("Press enter to exit...")
--- functions.py
+++ functions.py
@@ -1,12 +1,13 @@
-import time
-import multiprocessing
+import time, multiprocessing, os
 
+# Function for testing prime value
 def isPrime(x):
     for fraction in range(2,x):
         if x%fraction == 0:
             return False
     return True      
 
+# Generator of prime nubers candidates list _OLD_
 def gen_primes(start, end):
     primes = set()
     for n in range(start, end):
@@ -14,6 +15,7 @@ def gen_primes(start, end):
             primes.add(n)
             yield n
 
+# Switch for optimal factorization method based on modulo size
 def factorization_switch(modulo):
     bit_size = len(str(bin(modulo).replace("0b", ""))) #size of modulo in bit
     if bit_size < 10:
@@ -22,28 +24,29 @@ def factorization_switch(modulo):
         factors, factorization_time = factorization_multi(modulo, bit_size)
     return factors, factorization_time
 
+# Function for factorization with single process
 def factorization_basic(modulo):
     start_time = time.time()
     factors = []
     for factor in range(3, modulo, 2):
         if modulo % factor == 0:
-            factors[0] = factor
-            factors[1] = int(modulo/factor)
+            factors.append(factor)
+            factors.append(int(modulo/factor))
             end_time = time.time()
             return factors, end_time - start_time
-    raise Exception("The modulo {} isn't created by two factors!".format(modulo))
-# Function for multiprocess factorization
+    print("\n\nFatal Error: Modulo is not created by two prime factors!\n")
+    os._exit(0)
+
+# Function compatible with multiprocess factorization
 def factorization_core(modulo, start, end, results):
         for factor in range(start, end, 2):
             if modulo % factor == 0:
-                #print("factor WAS found in range of [{},{}]".format(start, end))
                 results[0] = factor
                 results[1] = int(modulo / factor)
                 return 1
-
-        #print("factor was NOT found in range of [{},{}]".format(start, end))
         return 0
 
+# Function for preparing multiprocess factorization
 def factorization_multi(modulo, mod_size):
     start_time = time.time()
     
@@ -54,22 +57,22 @@ def factorization_multi(modulo, mod_size):
     else:
         start = 2 ** ((mod_size/2) - 1)
         end = 2 ** (mod_size/2)
+
     # Making sure start, end are odd numbers
     if start%2 == 0:
         start -= 1
 
-    if end%2 == 0:
-        end -= 1
     # Assigning
     start_p1 = 3
     end_p1 = int(start)
     start_p2 = int(start)
-    end_p2 = (int(end) + 2) # +1 because of range() and another +1 because if end%2 == 0:
+    end_p2 = (int(end) + 1) # +1 because of range()
      
 
     if __name__ == "functions":
-        results = multiprocessing.Array('i', 2)          # Make shared memory
+        results = multiprocessing.Array('i', 2) # Make shared memory
 
+        # Loop for initiating both processes with diferent parameters
         for count in range(1, 3):
             if count == 1:
                 process = multiprocessing.Process(target=factorization_core, args=[modulo, start_p1, end_p1, results])
@@ -77,19 +80,23 @@ def factorization_multi(modulo, mod_size):
             if count == 2:
                 process = multiprocessing.Process(target=factorization_core, args=[modulo, start_p2, end_p2, results])  
         
+        # Starting both processes 
         process.start()
+
         # Catching main thread and waiting for both results
         while True:
-            if results[1] != 0:
+            if results[1] != 0:    ################### Warning! possible infinity loop if results not found #################
                 break
+
         # Terminating any left processes 
         if process.is_alive():
             process.terminate()
+
         # Returning results
         end_time = time.time()
         return results, end_time - start_time
 
-
+# Function for finding inversion in modulo _OLD_
 def inversion(public_key, PHI):
     private_key = 1
     if isPrime(PHI) == True:
@@ -101,10 +108,12 @@ def inversion(public_key, PHI):
             else:
                 private_key += 1
 
+# Function of Euler inversion _OLD_
 def eulerInversion(public_key, PHI):
     private_key = (public_key ** (PHI - 2))%PHI
     return private_key
 
+# Function for finding private key with Extended Euclidian Algorithm
 def extendedEuclidianAlgirithmInversion(PHI, public_key):
 	if PHI == 0:
 		return (0, 1)
@@ -112,6 +121,7 @@ def extendedEuclidianAlgirithmInversion(PHI, public_key):
 		x, y = extendedEuclidianAlgirithmInversion(public_key % PHI, PHI)
 		return (y - (public_key//PHI) * x, x)
 
+# Function checking if private key is negative value and converting it to positive value
 def signCheck(private_key, public_key, PHI):
     if (private_key*public_key)%PHI == 1:
         if private_key < 0:
@@ -122,3 +132,22 @@ def signCheck(private_key, public_key, PHI):
         if private_key < 0:
             return private_key%PHI
         return (private_key)
+
+# Function checking if convertion to int is possible
+def numCheck(modulo, public_key):
+
+    if len(modulo) == 0 or len(public_key) == 0:
+        print(len(modulo))
+        return False
+
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+    for i in modulo:
+        if numbers.count(i) == 0:
+            return False
+
+    for i in public_key:
+        if numbers.count(i) == 0:
+            return False
+    
+    return True
